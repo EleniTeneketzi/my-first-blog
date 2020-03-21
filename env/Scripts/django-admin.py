64e1985ff7f12f31ede3ad0380d2ab7be519b6f9
lena <eleni.teneketzi@gmail.com>
2020-03-21 12:40:57 +0100

    My Django Girls app, first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\pituf\Desktop\ETi\6-Python\Workspace\djangogirls\env\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
